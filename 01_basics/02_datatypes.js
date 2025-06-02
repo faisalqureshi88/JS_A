d28c9d8abb2451fed18abfe76b5a6055d487c633
faisalqureshi88 <qureshi.faisal8893@gmail.com>
2025-06-02 19:16:07 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer verion
+
+// alert("3 + 3") // we are using node js not browser
+
+console.log(3 + 3); // code readibility that should be high
+
+let name = "faisal"
+let age = 34
+let isloggedin = false
+
+/* primitive data types */
+// number => 2 to power 53
+// String = ""
+// Boolean = T/F
+// null = standalone value(not represent value)
+// undefined = not defined
+// symbol = A Symbol is a unique and immutable primitive value
+
+/* Non primitive data types */
+//object
+//array
+//function
