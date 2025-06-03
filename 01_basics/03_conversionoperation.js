d27b85f86f141ad124be1e2c9bce96b5a89ecb4a
faisalqureshi88 <qureshi.faisal8893@gmail.com>
2025-06-03 15:43:39 +0000

    Prefix and Psotfix

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -25,6 +25,55 @@
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
 
+//************************** Operations ****************************** */
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+//console.log(2+2);
+
+//console.log(2-2);
+
+//console.log(2*2);
+
+//console.log(2**2);
+
+//console.log(2/3);
+
+//console.log(2%2);
+
+
+let str1 = "Faisal"
+let str2 = " Qureshi"
+str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" +2);
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log((3 + 4) * 5);
+
+
+// console.log(+true)
+// console.log(+false)
+
+// console.log(true+) // it gives error
+// console.log(false+) // it gives error
+
+// let num1, num2, num3
+
+
+let gamecounterPreInc = 100
+++gamecounterPreInc;
+console.log(gamecounterPreInc);
+
+let gamecounterPreDec = 100
+--gamecounterPreDec;
+console.log(gamecounterPreDec);
