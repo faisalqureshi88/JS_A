8daeee39fb7ae1401a0044c957048c5cf3b89b1e
faisalqureshi88 <qureshi.faisal8893@gmail.com>
2025-06-03 19:15:01 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+//***Avoid null and undefined comparision***//
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+
+//=== strick check, check values and datatype both//
+
+
+
