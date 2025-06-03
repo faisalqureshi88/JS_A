3738d29d8a53724f4fba9dd27e0cc7067f7dd73e
faisalqureshi88 <qureshi.faisal8893@gmail.com>
2025-06-03 19:05:06 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+// Primitive Type (Call by value)
+// 7 types: String, Number, Boolean, null, undefined, symbol(Unique Value), BigInt  
+
+// Java Script is a dynamically typed language.
+
+
+// Non Primitive/ Reference Type (call by reference)
+// Objects, Array, Functions
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = true
+const outsideTemp = null
+let useremail;
+const id = Symbol('123')
+const anotherID = Symbol('123')
+
+console.log(id === anotherID);
+console.log(typeof id);
+console.log(typeof anotherID);
+
+
+
+const heros = ["Shaktiman", "Naagraj", "doga"];
+let myObj = {
+name: = "Faisal";
+age = 22;
+}
+
+const myFunction = function(){
+console.log("Hello World!");
+}
+
