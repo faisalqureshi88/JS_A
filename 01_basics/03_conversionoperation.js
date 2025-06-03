adf86bc1a62631f109a7c6c63f399e82dcacab8f
faisalqureshi88 <qureshi.faisal8893@gmail.com>
2025-06-03 15:10:04 +0000

    conersion of data types

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,30 @@
+//let score = "Faisal"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 1
+
+
+//let isLoggedIn = "Faisal"
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0=> false
+// empty => false
+// "hitesh" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
